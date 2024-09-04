b83db102836fe34db3691c1f8b703fc0ba2ab455
jiaoyinyi <zaoyihuang@gmail.com>
2024-09-04 18:38:14 +0800

    提交代码

---
+++ src/maps_diff.erl
@@ -0,0 +1,186 @@
+%%%-------------------------------------------------------------------
+%%% @author jiaoyinyi
+%%% @copyright (C) 2024, <COMPANY>
+%%% @doc
+%%% maps比较
+%%% @end
+%%% Created : 2024-06-24 00:00:00
+%%%-------------------------------------------------------------------
+-module(maps_diff).
+-export([
+    diff/2
+    , merge/1, merge/2
+]).
+
+%% 对比情况
+%% 值类型是基础类型，直接对比
+%% 值类型是数据类型，遍历对比
+%% 值类型是对象，递归对比
+
+%% maps对比信息
+-record(maps_diff, {
+    op :: add | delete | update
+    , old
+    , new
+}).
+
+%% @doc 比对
+-spec diff(map() | list(), map() | list()) -> map().
+diff(OldMap, NewMap) when is_map(OldMap) andalso is_map(NewMap) ->
+    diff_map(OldMap, NewMap);
+diff(OldList, NewList) when is_list(OldList) andalso is_list(NewList) ->
+    diff_list(OldList, NewList).
+
+diff_map(OldMap, NewMap) ->
+    DiffMap = diff_map_delete(OldMap, NewMap, #{}),
+    diff_map_add_update(OldMap, NewMap, DiffMap).
+
+diff_map_delete(OldMap, NewMap, DiffMap) ->
+    maps:fold(
+        fun(Key, Val, Acc) ->
+            case maps:is_key(Key, NewMap) of
+                false ->
+                    Diff = #maps_diff{op = delete, old = Val},
+                    maps:put(Key, Diff, Acc);
+                true ->
+                    Acc
+            end
+        end, DiffMap, OldMap
+    ).
+
+diff_map_add_update(OldMap, NewMap, DiffMap) ->
+    maps:fold(
+        fun(Key, Val, Acc) ->
+            case maps:find(Key, OldMap) of
+                {ok, Val} -> %% 相同
+                    Acc;
+                {ok, OldVal} -> %% 更新
+                    Diff = diff_update(OldVal, Val),
+                    maps:put(Key, Diff, Acc);
+                error -> %% 新增
+                    Diff = #maps_diff{op = add, new = Val},
+                    maps:put(Key, Diff, Acc)
+            end
+        end, DiffMap, NewMap
+    ).
+
+diff_list(OldList, NewList) ->
+    diff_list(OldList, NewList, #{}, 0).
+
+diff_list([], [], DiffMap, _Idx) ->
+    DiffMap;
+diff_list([I | OldList], [I | NewList], DiffMap, Idx) ->
+    diff_list(OldList, NewList, DiffMap, Idx + 1);
+diff_list([OldI | OldList], [NewI | NewList], DiffMap, Idx) ->
+    Diff = diff_update(OldI, NewI),
+    NewDiffMap = maps:put(Idx, Diff, DiffMap),
+    diff_list(OldList, NewList, NewDiffMap, Idx + 1);
+diff_list([OldI | OldList], [], DiffMap, Idx) ->
+    Diff = #maps_diff{op = delete, old = OldI},
+    NewDiffMap = maps:put(Idx, Diff, DiffMap),
+    diff_list(OldList, [], NewDiffMap, Idx + 1);
+diff_list([], [NewI | NewList], DiffMap, Idx) ->
+    Diff = #maps_diff{op = add, new = NewI},
+    NewDiffMap = maps:put(Idx, Diff, DiffMap),
+    diff_list([], NewList, NewDiffMap, Idx + 1).
+
+diff_update(OldVal, NewVal) when is_map(OldVal) andalso is_map(NewVal) ->
+    diff_map(OldVal, NewVal);
+diff_update(OldVal, NewVal) when is_list(OldVal) andalso is_list(NewVal) ->
+    diff_list(OldVal, NewVal);
+diff_update(OldVal, NewVal) ->
+    #maps_diff{op = update, old = OldVal, new = NewVal}.
+
+%% @doc 合并对比
+-spec merge([map()]) -> map().
+merge([DiffMap]) ->
+    DiffMap;
+merge([OldDiffMap, NewDiffMap | DiffMaps]) ->
+    merge([merge(OldDiffMap, NewDiffMap) | DiffMaps]).
+
+merge(OldDiffMap, NewDiffMap) ->
+    maps:fold(
+        fun(Key, Diff, Acc) ->
+            case maps:find(Key, Acc) of
+                {ok, NewDiff0} ->
+                    case merge_diff(Diff, NewDiff0) of
+                        remove ->
+                            maps:remove(Key, Acc);
+                        NewDiff ->
+                            maps:put(Key, NewDiff, Acc)
+                    end;
+                error ->
+                    maps:put(Key, Diff, Acc)
+            end
+        end, NewDiffMap, OldDiffMap
+    ).
+
+%% {"data" => {"hello" => #maps_diff{}}}
+%% {"data" => #maps_diff{}}
+
+%% {"data" => #maps_diff{op=add,new={}}}
+%% {"data" => {hello => #maps_diff{op=add,new=1}}}
+
+merge_diff(#maps_diff{op = add}, #maps_diff{op = update, new = NewVal}) -> %% 新增操作合并更新操作，更新新增操作的值
+    #maps_diff{op = add, new = NewVal};
+merge_diff(#maps_diff{op = add}, #maps_diff{op = delete}) -> %% 新增操作合并删除操作，移除该操作
+    remove;
+merge_diff(#maps_diff{op = update, old = OldVal}, #maps_diff{op = update, new = NewVal}) when OldVal =:= NewVal -> %% 更新操作合并更新操作，且更新回原值
+    remove;
+merge_diff(#maps_diff{op = update, old = OldVal}, #maps_diff{op = update, new = NewVal}) -> %% 更新操作合并更新操作，更新更新操作的值
+    #maps_diff{op = update, old = OldVal, new = NewVal};
+merge_diff(#maps_diff{op = update}, #maps_diff{op = delete, old = OldVal}) -> %% 更新操作合并删除操作，更新为删除操作
+    #maps_diff{op = delete, old = OldVal};
+merge_diff(#maps_diff{op = delete, old = OldVal}, #maps_diff{op = add, new = NewVal}) when OldVal =:= NewVal -> %% 删除操作合并新增操作，更新为新增操作
+    remove;
+merge_diff(#maps_diff{op = delete, old = OldVal}, #maps_diff{op = add, new = NewVal}) -> %% 删除操作合并新增操作，更新为新增操作
+    #maps_diff{op = update, old = OldVal, new = NewVal};
+merge_diff(OldDiff, #maps_diff{op = update, old = OldVal, new = NewVal}) when is_map(OldDiff) -> %% 子值操作合并更新操作，更新更新操作的值
+    #maps_diff{op = update, old = OldVal, new = NewVal};
+merge_diff(OldDiff, #maps_diff{op = delete, old = OldVal}) when is_map(OldDiff) -> %% 子值操作合并删除操作，更新为删除操作
+    #maps_diff{op = delete, old = OldVal};
+merge_diff(#maps_diff{op = add, new = NewVal0}, NewDiff) when is_map(NewVal0) andalso is_map(NewDiff) -> %% 新增操作合并子值操作，更新新增操作的值
+    Ret =
+        case merge_diff_vals(NewVal0, NewDiff) of
+            remove ->
+                remove;
+            NewVal ->
+                #maps_diff{op = add, new = NewVal}
+        end,
+    Ret;
+merge_diff(#maps_diff{op = update, old = OldVal, new = NewVal0}, NewDiff) when is_map(NewVal0) andalso is_map(NewDiff) -> %% 更新操作合并子值操作，更新更新操作的值
+    case merge_diff_vals(NewVal0, NewDiff) of
+        remove ->
+            remove;
+        NewVal ->
+            #maps_diff{op = update, old = OldVal, new = NewVal}
+    end;
+merge_diff(OldDiff, NewDiff) when is_map(OldDiff) andalso is_map(NewDiff) -> %% 子值操作合并子值操作，递归按前面规则合并处理
+    merge(OldDiff, NewDiff).
+
+merge_diff_vals(ValMap, DiffMap) ->
+    Ret = maps:fold(
+        fun(Key, Diff, Acc) ->
+            merge_diff_val(Acc, Key, Diff)
+        end, ValMap, DiffMap
+    ),
+    case Ret =:= #{} of
+        true ->
+            remove;
+        _ ->
+            Ret
+    end.
+merge_diff_val(ValMap, Key, #maps_diff{op = add, new = Val}) ->
+    maps:put(Key, Val, ValMap);
+merge_diff_val(ValMap, Key, #maps_diff{op = update, new = Val}) ->
+    maps:put(Key, Val, ValMap);
+merge_diff_val(ValMap, Key, #maps_diff{op = delete}) ->
+    maps:remove(Key, ValMap);
+merge_diff_val(ValMap, Key, DiffMap) when is_map(DiffMap) -> %% 子值还是maps格式，递归处理 #{<<"data">> => #{<<"int">> => 1},<<"msg">> => <<"hello">>} #{<<"data">> => #{<<"float">> => {maps_diff,add,undefined,1.2}, <<"int">> => {maps_diff,update,1,2}}}
+    Val = maps:get(Key, ValMap),
+    case merge_diff_vals(Val, DiffMap) of
+        remove ->
+            maps:remove(Key, ValMap);
+        NewVal ->
+            maps:put(Key, NewVal, ValMap)
+    end.
---
+++ test/maps_diff_SUITE.erl
@@ -0,0 +1,60 @@
+%%%-------------------------------------------------------------------
+%%% @author jiaoyinyi
+%%% @copyright (C) 2024, <COMPANY>
+%%% @doc
+%%% maps比较简单测试
+%%% @end
+%%% Created : 2024-06-24 00:00:00
+%%%-------------------------------------------------------------------
+-module(maps_diff_SUITE).
+
+-include_lib("common_test/include/ct.hrl").
+-include_lib("eunit/include/eunit.hrl").
+
+%% API
+-compile(export_all).
+
+all() ->
+    [
+        diff_test
+        , merge_test
+    ].
+
+diff_test(Config) ->
+    Map1 = #{<<"a">> => 10, <<"b">> => [5, 10, 20, 30], <<"c">> => #{<<"msg">> => <<"hello">>, <<"del">> => 10}, <<"del">> => 10},
+    Map2 = #{<<"a">> => 10, <<"b">> => [10, 20, 30, 40, 50], <<"c">> => #{<<"msg">> => <<"hello234">>, <<"new">> => 10}, <<"new">> => 10},
+    Diff = #{<<"b">> =>
+    #{0 => {maps_diff, update, 5, 10},
+        1 => {maps_diff, update, 10, 20},
+        2 => {maps_diff, update, 20, 30},
+        3 => {maps_diff, update, 30, 40},
+        4 => {maps_diff, add, undefined, 50}},
+        <<"c">> =>
+        #{<<"del">> => {maps_diff, delete, 10, undefined},
+            <<"msg">> => {maps_diff, update, <<"hello">>, <<"hello234">>},
+            <<"new">> => {maps_diff, add, undefined, 10}},
+        <<"del">> => {maps_diff, delete, 10, undefined},
+        <<"new">> => {maps_diff, add, undefined, 10}},
+    Diff = maps_diff:diff(Map1, Map2),
+    Config.
+
+merge_test(Config) ->
+    Map1 = #{<<"a">> => 10, <<"b">> => [5, 10], <<"del">> => 10},
+    Map2 = #{<<"a">> => 10, <<"b">> => [10, 20], <<"c">> => #{<<"msg">> => <<"hello">>, <<"data">> => 10}, <<"new">> => 10},
+    Map3 = #{<<"a">> => 10, <<"b">> => [10, 20], <<"c">> => #{<<"msg">> => <<"hello">>}},
+    Map4 = #{<<"a">> => 10, <<"b">> => [10, 20], <<"c">> => #{<<"msg">> => <<"hello">>, <<"data">> => #{<<"int">> => 1}}},
+    Map5 = #{<<"a">> => 10, <<"b">> => [10, 20], <<"c">> => #{<<"msg">> => <<"hello">>, <<"data">> => #{<<"int">> => 2, <<"float">> => 1.2}}},
+    Diff1 = maps_diff:diff(Map1, Map2),
+    Diff2 = maps_diff:diff(Map2, Map3),
+    Diff3 = maps_diff:diff(Map3, Map4),
+    Diff4 = maps_diff:diff(Map4, Map5),
+    Merge = #{<<"b">> =>
+    #{0 => {maps_diff, update, 5, 10},
+        1 => {maps_diff, update, 10, 20}},
+        <<"c">> =>
+        {maps_diff, add, undefined,
+            #{<<"data">> => #{<<"float">> => 1.2, <<"int">> => 2},
+                <<"msg">> => <<"hello">>}},
+        <<"del">> => {maps_diff, delete, 10, undefined}},
+    Merge = maps_diff:merge([Diff1, Diff2, Diff3, Diff4]),
+    Config.
